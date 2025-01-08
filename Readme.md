be3ad802361ef33106cd776ae54272150e7aa1eb
Michael Jackson <miglja619@gmail.com>
2025-01-08 16:02:26 -0500

    Add Readme.md

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Phrase object (with palindrome detector)
+
+This is a sample NPM module created in [*Learn Enough JavaScript to Be Dangerous*](https://www.learnenough.com/javascript-tutorial) by Michael Hartl.
+
+The module can be used as follows:
+
+```
+$ npm install --global mgj-palindrome
+$ vim test.js
+let Phrase = require("mgj-palindrome");
+let napoleonsLament = new Phrase("Able was I, ere I saw Elba.");
+console.log(napoleonsLament.palindrome());
+$ node test.js
+true
+```
--- index.js
+++ index.js
@@ -1,39 +1,18 @@
 
-// Reverses a string
-/*function reverse(string) {
-    return Array.from(string).reverse().join("");
-}*/
+module.exports = Phrase;
 
 // Adds 'reverse' to all strings.
 String.prototype.reverse = function() {
     return Array.from(this).reverse().join("");
 }
 
-String.prototype.blank = function() {
-    return !!this.match(/^\s+$/);
-}
-
-Array.prototype.last = function() {
-    return this[this.length-1];
-}
-
-// Returns true for palindromes, false otherwise.
-function palindrome(string) {
-    let processedContent = string.toLowerCase();
-    return processedContent == reverse(processedContent);
-}
-
 // Defines a phrase object.
 function Phrase(content) {
     this.content = content;
 
-    this.processor = function(string) {
-        return string.toLowerCase();
-    }
-
     // Returns content processed for palidrome testing
     this.processedContent = function processedContent() {
-        return this.processor(this.content);
+        return this.content.toLowerCase();
     }
 
     // Returns true if the phrase is a palindrome,
@@ -41,19 +20,5 @@ function Phrase(content) {
     this.palindrome = function palindrome() {
         return this.processedContent() === 
             this.processedContent().reverse();
-    };
-}
-
-// Defines a TranslatedPhrase object
-function TranslatedPhrase(content, translation) {
-    this.content = content;
-    this.translation = translation;
-
-    // Returns translation processed for palindrome
-    // testing.
-    this.processedContent = function processedContent() {
-        return this.processor(this.translation);
     }
 }
-
-TranslatedPhrase.prototype = new Phrase();
